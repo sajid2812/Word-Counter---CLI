4b601080cecd30b4d35e3c12476bc7413b80ce5d
sajid-edninja <ssk73573@gmail.com>
2025-02-16 19:02:40 +0530

    fix count command

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ program
   .description("CLI to count the number of words inside the given file path")
   .version("1.00.00");
 
-program.argument("<string>", "file path").action((filePath) => {
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const wordsCount = data.trim().split(/\s+/).length;
-      console.log(`You have ${wordsCount} words in this file`);
-    }
+program
+  .command("count")
+  .description("Count the number of words in a file")
+  .argument("<string>", "file path")
+  .action((filePath) => {
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const wordsCount = data.trim().split(/\s+/).length;
+        console.log(`You have ${wordsCount} words in this file`);
+      }
+    });
   });
-});
 
 program.parse();
