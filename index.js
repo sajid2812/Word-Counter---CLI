300e2efedce6117ab6f7aa3019a50d13f0b66d61
sajid-edninja <ssk73573@gmail.com>
2025-02-16 10:06:00 +0530

    add custom cli

---
+++ index.js
@@ -0,0 +1,22 @@
+// Create a command line interface that lets the user specify a file path and the nodejs process counts the number of words inside it.
+const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .name("word-counter")
+  .description("CLI to count the number of words inside the given file path")
+  .version("1.00.00");
+
+program.argument("<string>", "file path").action((filePath) => {
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const wordsCount = data.trim().split(/\s+/).length;
+      console.log(`You have ${wordsCount} words in this file`);
+    }
+  });
+});
+
+program.parse();
